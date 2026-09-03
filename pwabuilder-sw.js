70f2fe86503e9d36fbb407df5be85d11b9114235
monsieourlamont <vigiland175@gmail.com>
2026-09-02 23:33:11 -0500

    Add files via upload

---
+++ pwabuilder-sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "obj-detect-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // Dejamos que la red maneje casi todo por ahora
+});
